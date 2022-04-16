683d8286cb4bfa67d261bc5814d3f9a058d219b3
Eric J. Wan <ericwan957@gmail.com>
2022-04-16 16:48:10 -0400

    Add fraction class

---
+++ src/Fraction.js
@@ -0,0 +1,40 @@
+class Fraction {
+  constructor(num, denom) {
+    if (denom === 0)
+      throw Error("Fraction with 0 denominator is illegal");
+
+    this.#num = num;
+    this.#denom = denom;
+  }
+
+  static add(a, b) {
+    return new Fraction(a.#num * b.#denom + b.#num * a.#denom,
+                        a.#denom * b.#denom);
+  }
+
+  toString() {
+    const f = this.reduce();
+    if (f.#denom === 1)
+      return f.#num.toString();
+    return `${f.#num}/${f.#denom}`;
+  }
+
+  reduce() {
+    const factor = Fraction.#gcd(this.#num, this.#denom);
+    const f = new Fraction(this.#num / factor, this.#denom / factor);
+    if (f.#denom < 0) {
+      f.#num *= -1;
+      f.#denom *= -1;
+    }
+    return f;
+  }
+
+  #num;
+  #denom;
+
+  static #gcd(a, b) {
+    if (b === 0)
+      return a;
+    return Fraction.#gcd(b, a % b);
+  }
+}
